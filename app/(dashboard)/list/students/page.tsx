d63d7388e31ace01fd8c217e7e5e092f01e7bdea
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-09-01 00:16:23 +0530

    singleStudentPage and studentListPage added

--- app/(dashboard)/list/students/page.tsx
+++ app/(dashboard)/list/students/page.tsx
@@ -74,16 +74,16 @@ const StudentListPage = () => {
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
+          <Link href={`/list/students/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-mahtoSky">
               <Image src="/view.png" alt="" width={16} height={16} />
             </button>
           </Link>
           {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-mahtoPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
-            // <FormModal table="student" type="delete" id={item.id} />
+            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-mahtoPurple">
+            //   <Image src="/delete.png" alt="" width={16} height={16} />
+            // </button>
+            <FormModal table="student" type="delete" id={item.id} />
           )}
         </div>
       </td>
@@ -105,10 +105,10 @@ const StudentListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-mahtoYellow">
-                <Image src="/plus.png" alt="" width={14} height={14} />
-              </button>
-              // <FormModal table="student" type="create" />
+              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-mahtoYellow">
+              //   <Image src="/plus.png" alt="" width={14} height={14} />
+              // </button>
+              <FormModal table="student" type="create" />
             )}
           </div>
         </div>
